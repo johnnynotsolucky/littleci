8e03b65351c7b1b393411743529f16275d783482
Tyrone Tudehope <tyronetudehope@gmail.com>
2020-01-29 20:29:59 +0200

    Update packages

--- build/build.sh
+++ build/build.sh
@@ -4,7 +4,6 @@ set -e
 # TODO build the correct branch when necessary
 git pull
 
-rustup component add rustfmt
 cargo +nightly fmt --all -- --check
 
 (cd swagger/ && npm ci && npm run build)
@@ -12,7 +11,7 @@ cargo +nightly fmt --all -- --check
 ./build/docker/build-images.sh
 
 cross build --target x86_64-unknown-linux-gnu --release
-cross build --target arm-unknown-linux-gnueabihf --release
+# cross build --target arm-unknown-linux-gnueabihf --release
 
 # TODO
 # cross build --target x86_64-pc-windows-gnu --release
